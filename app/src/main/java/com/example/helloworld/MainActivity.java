f662d80e7baeec6722fb61307d4d2b446d677703
Ishika Yadav <ishikayadav377@gmail.com>
2020-05-28 17:17:49 -0400

    Test Code

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //This is my test code//
+        //This is my test code.//
     }
 }
